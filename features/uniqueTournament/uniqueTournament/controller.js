c4b48c6cb19eea169e24108eb88da1f8fd0e2f5c
harshLogicgo <harsh.logicgo@gmail.com>
2024-07-01 10:32:19 +0530

    uniqueTournament data

--- features/uniqueTournament/uniqueTournament/controller.js
+++ features/uniqueTournament/uniqueTournament/controller.js
@@ -8,6 +8,7 @@ import Season from "./models/seasonsSchema.js";
 import TopPlayers from "./models/topPlayesSchema.js";
 import FeaturedMatches from "./models/topPlayesSchema.js";
 import SeasonStanding from "./models/standingSchema.js";
+import LeagueMatches from "./models/leagueMatchesSchema.js";
 
 const getTournamentById = async (req, res, next) => {
   try {
@@ -281,11 +282,7 @@ const getSeasonStandingByTournament = async (req, res, next) => {
         if (season) {
           data = season.data;
         } else {
-          data = await service.getSeasonStandingByTournament(
-            id,
-            seasonId,
-            type
-          );
+          data = await service.getSeasonStandingByTournament(id, seasonId, type);
           cacheService.setCache(key, data, cacheTTL.TEN_SECONDS);
           seasonStanding.seasons.push({ seasonId, type, data: data });
           await seasonStanding.save();
@@ -326,11 +323,11 @@ const getSeasonStandingByTournament = async (req, res, next) => {
                 id: "$$rowObj.team.id",
                 shortName: "$$rowObj.team.shortName",
                 teamName: "$$rowObj.team.name",
-              },
-            },
-          },
-        },
-      },
+              }
+            }
+          }
+        }
+      }
     ]);
 
     return apiResponse({
@@ -359,6 +356,7 @@ const getSeasonStandingByTournament = async (req, res, next) => {
     }
   }
 };
+
 const getSeasonTopPlayersByTournament = async (req, res, next) => {
   try {
     const { id, seasonId, positionDetailed } = req.params;
@@ -406,16 +404,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
       }
     }
 
-    const transformedData = data.reduce((acc, item) => {
-      Object.keys(item).forEach((key) => {
-        if (!acc[key]) {
-          acc[key] = [];
-        }
-        acc[key].push(...item[key]);
-      });
-      return acc;
-    }, {});
-
     const teamPlayerData = await TopPlayers.aggregate([
       { $match: { tournamentId: id } },
       {
@@ -449,8 +437,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             runsScored: "$$run.statistics.runsScored",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -497,8 +483,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                             battingStrikeRate:
                               "$$run.statistics.battingStrikeRate",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -544,8 +528,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             battingAverage: "$$run.statistics.battingAverage",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -585,8 +567,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             fifties: "$$run.statistics.fifties",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -626,8 +606,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             hundreds: "$$run.statistics.hundreds",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -667,8 +645,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             fours: "$$run.statistics.fours",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -708,8 +684,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             sixes: "$$run.statistics.sixes",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -749,8 +723,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             nineties: "$$run.statistics.nineties",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -796,8 +768,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             bowlingAverage: "$$run.statistics.bowlingAverage",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -843,8 +813,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             fiveWicketsHaul: "$$run.statistics.fiveWicketsHaul",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -883,8 +851,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                           in: {
                             economy: "$$run.statistics.economy",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -931,8 +897,6 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
                             bowlingStrikeRate:
                               "$$run.statistics.bowlingStrikeRate",
                             player: "$$run.player.name",
-                            playerId: "$$run.player.id",
-                            position: "$$run.player.position",
                           },
                         },
                       },
@@ -945,6 +909,8 @@ const getSeasonTopPlayersByTournament = async (req, res, next) => {
         },
       },
     ]);
+
+
 
     return apiResponse({
       res,
@@ -981,20 +947,98 @@ const getSeasonMatchesByTournament = async (req, res, next) => {
 
     let data = cacheService.getCache(key);
 
-    if (!data) {
-      data = await service.getSeasonMatchesByTournament(
-        id,
-        seasonId,
-        span,
-        page
-      );
+    const leagueMatchesData = await LeagueMatches.findOne({ tournamentId: id });
+    const findMatches = leagueMatchesData?.seasons?.find(season => season.seasonId === seasonId)
+    const count = Math.ceil(findMatches?.data?.length / 10);
+    const adjustedPage = Math.floor((page - 1) / 3);
 
-      cacheService.setCache(key, data, cacheTTL.ONE_HOUR);
+    if (!data || page > count) {
+      if (leagueMatchesData) {
+        if (findMatches) {
+          if(page <= count){
+            console.log("9999999999999999999")
+            data = findMatches.data;
+          } else {
+            const newData = await service.getSeasonMatchesByTournament(id, seasonId, span, adjustedPage);
+              // console.log("findMatches", findMatches)
+            const existingEvents = findMatches.data.map((event) => event.id );
+            const uniqueEvents = newData.events.filter((event) => !existingEvents.includes(event.id));
+            findMatches.data.push(...uniqueEvents);
+            await leagueMatchesData.save();
+            data = findMatches.data;
+          }
+        } else {
+          data = await service.getSeasonMatchesByTournament(id, seasonId, span, adjustedPage);
+          cacheService.setCache(key, data, cacheTTL.TEN_SECONDS);
+          leagueMatchesData.seasons.push({ seasonId, data: data.events }); 
+          await leagueMatchesData.save();
+        }
+      } else {
+        const newData = await service.getSeasonMatchesByTournament(id, seasonId, span, 0);
+        if (leagueMatchesData) {
+          // Filter out duplicate events
+          const existingEvents = leagueMatchesData.seasons.map((season) => season.data);
+          const uniqueEvents = newData.data.filter((event) => !existingEvents.includes(event.id));
+          // Push unique events to the existing data
+          leagueMatchesData.seasons.push(...uniqueEvents);
+          await leagueMatchesData.save();
+          data = leagueMatchesData;
+        } else {
+          // If no existing data, save the new data
+          const leagueMatchesEntry = new LeagueMatches({
+            tournamentId: id,
+            seasons: [{ seasonId: seasonId, data: newData.events }],
+          });
+          await leagueMatchesEntry.save();
+          data = leagueMatchesEntry;
+        }
+        cacheService.setCache(key, data, cacheTTL.TEN_SECONDS);
+
+      }
     }
+
+    const pageSize = 10;
+    const skip = (page - 1) * pageSize;
+
+    const aggregatedData = await LeagueMatches.aggregate([
+      { $match: { tournamentId: id, "seasons.seasonId": seasonId } },
+      { $unwind: "$seasons" },
+      { $match: { "seasons.seasonId": seasonId } },
+      { $unwind: "$seasons.data" },
+      { $skip: skip },
+      { $limit: pageSize },
+      {
+        $project: {
+          homeTeam: {
+            name: "$seasons.data.homeTeam.name",
+            score: "$seasons.data.homeScore.current",
+            wickets: "$seasons.data.homeScore.innings.inning1.wickets",
+            overs: "$seasons.data.homeScore.innings.inning1.overs",
+          },
+          awayTeam: {
+            name: "$seasons.data.awayTeam.name",
+            score: "$seasons.data.awayScore.current",
+            wickets: "$seasons.data.awayScore.innings.inning1.wickets",
+            overs: "$seasons.data.awayScore.innings.inning1.overs",
+          },
+          winner: {
+            $cond: {
+              if: { $eq: ["$seasons.data.winnerCode", 1] },
+              then: "$seasons.data.homeTeam.name",
+              else: "$seasons.data.awayTeam.name",
+            },
+          },
+          note: "$seasons.data.note",
+          endTimestamp: "$seasons.data.endTimestamp",
+          startTimestamp: "$seasons.data.startTimestamp",
+          id: "$seasons.data.id",
+        },
+      },
+    ]);
 
     return apiResponse({
       res,
-      data: data,
+      data: aggregatedData,
       status: true,
       message: "Season matches fetched successfully",
       statusCode: StatusCodes.OK,
