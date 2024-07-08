cda1b002267c5e51e58df84a4959e482e9242cf8
harshLogicgo <harsh.logicgo@gmail.com>
2024-07-08 16:29:22 +0530

    changes some data

--- features/matches/route.js
+++ features/matches/route.js
@@ -7,15 +7,14 @@ const route = express.Router();
 // GET routes
 // ----------------
 route.get("/standings", matchesController.getStandingsDetailsById);
+route.get("/overs", matchesController.getOverDetailsById);
 route.get("/:id", matchesController.getSingleMatchDetail);
 route.get("/:id/votes", matchesController.getMatchVotes);
-route.get("/overs/:matchId", matchesController.getOverDetailsById);
 route.get("/scorecard/:matchId", matchesController.getScoreCardDetailsById);
 route.get("/squad/:matchId", matchesController.getSquadDetailsById);
 route.get("/matches/:customId", matchesController.getMatchesScreenDetailsById);
 route.get("/:id/pregame-form", matchesController.getPregameForm);
 route.get("/:id/odds", matchesController.getMatchOdds);
 route.get("/:id/h2h", matchesController.getMatchH2H);
-
 
 export default route;
