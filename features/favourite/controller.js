3b230263b1399056b72405faa7da2ebae8819bb4
harshLogicgo <harsh.logicgo@gmail.com>
2024-07-17 12:47:35 +0530

    add favorite list and add API

--- features/favourite/controller.js
+++ features/favourite/controller.js
@@ -1,16 +1,9 @@
 import { apiResponse } from "../../helper/apiResponse.js";
 import { StatusCodes } from "http-status-codes";
-import cacheService from "../cache/service.js";
-import service from "./service.js";
-import cacheTTL from "../cache/constants.js";
-import {
-  filterLiveMatchData,
-  fractionalOddsToDecimal,
-  filterStandingsData,
-} from "../../websocket/utils.js";
 import FavouriteDetails from "./models/favouriteDetails.js";
 import FavouritePlayerDetails from "./models/favouritePlayerDetails.js";
 import FavouriteTeamDetails from "./models/favouriteTeamDetails.js";
+import favouriteLeagueDetails from "./models/favouriteLeagueDetails.js";
 
 const favouriteMatchesadd = async (req, res, next) => {
   try {
@@ -33,14 +26,18 @@ const favouriteMatchesadd = async (req, res, next) => {
         { type: type, status: newStatus }
       );
 
+      const allExistingFavourite = await FavouriteDetails.findOne({
+        matchesId: matchesId,
+      });
+
       return apiResponse({
         res,
-        data: existingFavourite,
+        data: allExistingFavourite,
         status: true,
         message:
           newStatus === false
-            ? "Favorite matches added successfully"
-            : "Favorite matches removed successfully",
+            ? "Favorite matches remove successfully"
+            : "Favorite matches added successfully",
         statusCode: StatusCodes.OK,
       });
     } else {
@@ -75,16 +72,18 @@ const favouriteMatchesadd = async (req, res, next) => {
 const favouriteMatcheslist = async (req, res, next) => {
   try {
     // Fetch data using populate and select
-    const favoriteMatchList = await FavouriteDetails.find()
+    const favoriteMatchList = await FavouriteDetails.find({ status: 1 })
       .populate({
         path: "matchesId",
         model: "MatcheDetailsByMatchScreen",
         select: "data",
       })
       .exec();
+    console.log(favoriteMatchList);
 
     // Map through favoriteMatchList to reshape data
     const reshapedData = favoriteMatchList.map((favourite) => {
+      console.log(favourite);
       return {
         tournament: {
           name: favourite.matchesId.data.event.tournament.name,
@@ -213,14 +212,18 @@ const favouritePlayersadd = async (req, res, next) => {
         { type: type, status: newStatus }
       );
 
+      const allExistingFavourite = await FavouritePlayerDetails.findOne({
+        playerId: playerId,
+      });
+
       return apiResponse({
         res,
-        data: existingFavourite,
+        data: allExistingFavourite,
         status: true,
         message:
           newStatus === false
-            ? "Favorite player added successfully"
-            : "Favorite player removed successfully",
+            ? "Favorite player removed successfully"
+            : "Favorite player added successfully",
         statusCode: StatusCodes.OK,
       });
     } else {
@@ -265,7 +268,9 @@ const favouritePlayersadd = async (req, res, next) => {
 const favouritePlayerlist = async (req, res, next) => {
   try {
     // Fetch data using populate and select
-    const favoritePlayerList = await FavouritePlayerDetails.find()
+    const favoritePlayerList = await FavouritePlayerDetails.find({
+      status: true,
+    })
       .populate({
         path: "playerId",
         model: "PlayerDetails",
@@ -405,14 +410,18 @@ const favouriteTeamsadd = async (req, res, next) => {
         { type: type, status: newStatus }
       );
 
+      const allExistingFavourite = await FavouriteTeamDetails.findOne({
+        teamId: teamId,
+      });
+
       return apiResponse({
         res,
-        data: existingFavourite,
+        data: allExistingFavourite,
         status: true,
         message:
           newStatus === false
-            ? "Favorite team added successfully"
-            : "Favorite team removed successfully",
+            ? "Favorite team removed successfully"
+            : "Favorite team added successfully",
         statusCode: StatusCodes.OK,
       });
     } else {
@@ -457,7 +466,7 @@ const favouriteTeamsadd = async (req, res, next) => {
 const favouriteTeamList = async (req, res, next) => {
   try {
     // Fetch data using populate and select
-    const favoriteTeamList = await FavouriteTeamDetails.find()
+    const favoriteTeamList = await FavouriteTeamDetails.find({ status: 1 })
       .populate({
         path: "teamId",
         model: "TeamDetails",
@@ -576,6 +585,204 @@ const favouriteTeamList = async (req, res, next) => {
   }
 };
 
+const favouriteLeagueadd = async (req, res, next) => {
+  try {
+    const leagueId = req.body.leagueId;
+    const userId = req.body.userId;
+    const type = req.body.type;
+
+    const existingFavourite = await favouriteLeagueDetails.findOne({
+      leagueId: leagueId,
+    });
+
+    if (existingFavourite) {
+      // Toggle the status field
+      const newStatus = existingFavourite.status === true ? false : true;
+
+      // Update the document with the new status and type
+      const updatedFavourite = await favouriteLeagueDetails.updateOne(
+        { leagueId: leagueId },
+        { type: type, status: newStatus }
+      );
+
+      const allexistingFavourite = await favouriteLeagueDetails.findOne({
+        leagueId: leagueId,
+      });
+
+      return apiResponse({
+        res,
+        data: allexistingFavourite,
+        status: true,
+        message:
+          newStatus === false
+            ? "Favorite league removed successfully"
+            : "Favorite league added successfully",
+        statusCode: StatusCodes.OK,
+      });
+    } else {
+      // If document does not exist, create a new one with status 1
+      const newFavourite = await favouriteLeagueDetails.create({
+        leagueId: leagueId,
+        userId: userId,
+        status: 1, // Set initial status to 1
+        type: type,
+        // Additional fields can be added here
+      });
+
+      return apiResponse({
+        res,
+        data: newFavourite,
+        status: true,
+        message: "Favorite league added successfully",
+        statusCode: StatusCodes.OK,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    if (error.response && error.response.status === 404) {
+      return apiResponse({
+        res,
+        data: null,
+        status: true,
+        message: "No league found",
+        statusCode: StatusCodes.OK,
+      });
+    } else {
+      return apiResponse({
+        res,
+        status: false,
+        message: "Internal server error",
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      });
+    }
+  }
+};
+
+const favouriteLeagueList = async (req, res, next) => {
+  try {
+    // Fetch data using populate and select
+    const favoriteTeamList = await favouriteLeagueDetails
+      .find({ status: 1 })
+      .populate({
+        path: "leagueId",
+        model: "LeagueTournamentList",
+        select: "data",
+      })
+      .exec();
+
+    // Map through favoriteMatchList to reshape data
+    const reshapedData = favoriteTeamList.map((favourite) => {
+      return {
+        team: {
+          name: favourite.leagueId.data[0].name,
+          slug: favourite.leagueId.data[0].slug,
+          id: favourite.leagueId.data[0].id,
+          categoryName: favourite.leagueId.data[0].category.name,
+          sportName: favourite.leagueId.data[0].category.sport.name,
+
+          // category: {
+          //   name: favourite.teamId.data.player.category.name,
+          //   slug: favourite.playerId.data.player.category.slug,
+          //   id: favourite.playerId.data.player.category.id,
+          //   country: favourite.playerId.data.player.category.country,
+          // },
+          // },
+          // customId: favourite.playerId.data.player.customId,
+          // homeTeam: {
+          //   name: favourite.playerId.data.player.homeTeam.name,
+          //   slug: favourite.playerId.data.player.homeTeam.slug,
+          //   shortName: favourite.playerId.data.player.homeTeam.shortName,
+          //   nameCode: favourite.playerId.data.player.homeTeam.nameCode,
+          //   id: favourite.playerId.data.player.homeTeam.id,
+        },
+        // awayTeam: {
+        //   name: favourite.playerId.data.player.awayTeam.name,
+        //   slug: favourite.playerId.data.player.awayTeam.slug,
+        //   shortName: favourite.playerId.data.player.awayTeam.shortName,
+        //   nameCode: favourite.playerId.data.player.awayTeam.nameCode,
+        //   id: favourite.playerId.data.player.awayTeam.id,
+        // },
+        // homeScore: {
+        //   current: favourite.playerId.data.player.homeScore.current,
+        //   display: favourite.playerId.data.player.homeScore.display,
+        //   innings: Object.entries(
+        //     favourite.playerId.data.player.homeScore.innings
+        //   ).map(([key, value]) => ({
+        //     key,
+        //     score: value.score,
+        //     wickets: value.wickets,
+        //     overs: value.overs,
+        //     runRate: value.runRate,
+        //   })),
+        // },
+        // awayScore: {
+        //   current: favourite.playerId.data.player.awayScore.current,
+        //   display: favourite.playerId.data.player.awayScore.display,
+        //   innings: Object.entries(
+        //     favourite.playerId.data.player.awayScore.innings
+        //   ).map(([key, value]) => ({
+        //     key,
+        //     score: value.score,
+        //     wickets: value.wickets,
+        //     overs: value.overs,
+        //     runRate: value.runRate,
+        //   })),
+        // },
+        // status: {
+        //   code: favourite.playerId.data.player.status.code,
+        //   description: favourite.playerId.data.player.status.description,
+        //   type: favourite.playerId.data.player.status.type,
+        // },
+        // season: {
+        //   name: favourite.playerId.data.player.season.name,
+        //   year: favourite.playerId.data.player.season.year,
+        //   id: favourite.playerId.data.player.season.id,
+        // },
+        // notes: favourite.playerId.data.player.note,
+        // currentBattingTeamId:
+        //   favourite.playerId.data.player.currentBattingTeamId,
+        // endTimestamp: favourite.playerId.data.player.endTimestamp,
+        // startTimestamp: favourite.playerId.data.player.startTimestamp,
+        // slug: favourite.playerId.data.player.slug,
+        // tvUmpireName: favourite.playerId.data.player.tvUmpireName,
+        // venue: favourite.playerId.data.player.venue,
+        // umpire1Name: favourite.playerId.data.player.umpire1Name,
+        // umpire2Name: favourite.playerId.data.player.umpire2Name,
+        // winnerCode: favourite.playerId.data.player.winnerCode,
+        // id: favourite.playerId.data.player.id,
+      };
+    });
+
+    if (favoriteTeamList) {
+      return apiResponse({
+        res,
+        data: reshapedData,
+        status: true,
+        message: "Favourite fetched successfully ",
+        statusCode: StatusCodes.OK,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    if (error.response && error.response.status === 404) {
+      return apiResponse({
+        res,
+        data: null,
+        status: true,
+        message: "No overs found",
+        statusCode: StatusCodes.OK,
+      });
+    } else {
+      return apiResponse({
+        res,
+        status: false,
+        message: "Internal server error",
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      });
+    }
+  }
+};
+
 export default {
   favouriteMatchesadd,
   favouritePlayersadd,
@@ -583,4 +790,6 @@ export default {
   favouriteMatcheslist,
   favouritePlayerlist,
   favouriteTeamList,
+  favouriteLeagueadd,
+  favouriteLeagueList,
 };
