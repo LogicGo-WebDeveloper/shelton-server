8e48f95499a3e6797e9fe55dbff8a39e9d5b9451
vijay <vijay.logicgo@gmail.com>
2024-07-18 12:07:17 +0530

    add chaining operator

--- features/player/controller.js
+++ features/player/controller.js
@@ -20,7 +20,6 @@ const getPlayerDetailsById = async (req, res, next) => {
     let image = cacheService.getCache(key);
 
     if (!data) {
-      data = await service.getPlayerById(id);
       const players = await PlayerDetails.findOne({ PlayerId: id });
 
       if (players) {
@@ -50,6 +49,8 @@ const getPlayerDetailsById = async (req, res, next) => {
           });
           filename = `${config.cloud.digitalocean.baseUrl}/${config.cloud.digitalocean.rootDirname}/${folderName}/${name}`;
         }
+
+        data = await service.getPlayerById(id);
 
         const playerEntry = new PlayerDetails({
           PlayerId: id,
@@ -93,7 +94,7 @@ const getPlayerDetailsById = async (req, res, next) => {
 
     return apiResponse({
       res,
-      data: teamPlayerData[0].players[0],
+      data: teamPlayerData[0]?.players[0],
       status: true,
       message: "Player details fetched successfully",
       statusCode: StatusCodes.OK,
