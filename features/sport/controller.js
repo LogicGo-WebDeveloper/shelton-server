6206b9a32d5dd23794bc34a786e6ed46a0b243f4
vijay <vijay.logicgo@gmail.com>
2024-07-20 10:36:08 +0530

    sorting and pagination in global search api

--- features/sport/controller.js
+++ features/sport/controller.js
@@ -19,148 +19,6 @@ import TeamDetails from "../team/models/teamDetailsSchema.js";
 
 const folderName = "country";
 
-// const getCountryLeagueList = async (req, res, next) => {
-//   try {
-//     const { sport } = req.params;
-//     const key = cacheService.getCacheKey(req);
-
-//     let data = cacheService.getCache(key);
-//     if (!data) {
-//       const countryLeagueListEntry = await CountryLeagueList.findOne({ sport });
-//       if (countryLeagueListEntry) {
-//         data = countryLeagueListEntry.data;
-//       } else {
-//         data = await service.getCountryLeagueList(sport);
-//         await Promise.all(
-//           data.map(async (item) => {
-//             let alpha2 = item.alpha2 || undefined;
-//             const flag = item.flag || undefined;
-//             const identifier = (alpha2 || flag).toLowerCase();
-
-//             if (identifier) {
-//               const baseUrl = `${config.cloud.digitalocean.baseUrl}/${config.cloud.digitalocean.rootDirname}/${folderName}/${identifier}.png`;
-//               try {
-//                 const response = await fetch(baseUrl);
-//                 if (response.status !== 200) {
-//                   item.image = null;
-//                 } else {
-//                   item.image = baseUrl;
-//                 }
-//                 // console.log({ identifier }, "==>> free");
-//               } catch (error) {
-//                 const response = await axiosInstance.get(
-//                   `/static/images/flags/${identifier}.png`,
-//                   {
-//                     responseType: "arraybuffer",
-//                   }
-//                 );
-//                 // console.log({ identifier }, "==>> paid");
-//                 const buffer = Buffer.from(response.data, "binary");
-
-//                 await uploadFile({
-//                   filename: `${config.cloud.digitalocean.rootDirname}/${folderName}/${identifier}.png`,
-//                   file: buffer,
-//                   ACL: "public-read",
-//                 });
-//                 const imageUrl = `${config.cloud.digitalocean.baseUrl}/${config.cloud.digitalocean.rootDirname}/${folderName}/${identifier}.png`;
-//                 item.image = imageUrl;
-//               }
-//             }
-//             return item;
-//           })
-//         );
-
-//         cacheService.setCache(key, data, cacheTTL.ONE_DAY);
-
-//         const fetchAllCategories = async () => {
-//           const promises = data.map(async (item) => {
-//             const response = await sportService.getLeagueTournamentList(
-//               item.id
-//             );
-//             item.tournamentlist = response;
-//             return item;
-//           });
-
-//           const results = await Promise.all(promises);
-//           return results;
-//         };
-//         data = await fetchAllCategories();
-
-//         const newCountryLeagueListEntry = new CountryLeagueList({
-//           sport,
-//           data,
-//         });
-//         await newCountryLeagueListEntry.save();
-//       }
-//     }
-
-//     const modifyData = await CountryLeagueList.aggregate([
-//       { $match: { sport: sport } },
-//       {
-//         $project: {
-//           data: {
-//             $map: {
-//               input: "$data",
-//               as: "dataObj",
-//               in: {
-//                 name: "$$dataObj.name",
-//                 slug: "$$dataObj.slug",
-//                 image: "$$dataObj.image",
-//                 id: "$$dataObj.id",
-//                 tournamentlist: {
-//                   $map: {
-//                     input: "$$dataObj.tournamentlist",
-//                     as: "tournament",
-//                     in: {
-//                       name: "$$tournament.name",
-//                       slug: "$$tournament.slug",
-//                       category: {
-//                         name: "$$tournament.category.name",
-//                         slug: "$$tournament.category.slug",
-//                         id: "$$tournament.category.id",
-//                         flag: "$$tournament.category.flag",
-//                       },
-//                       userCount: "$$tournament.userCount",
-//                       id: "$$tournament.id",
-//                     },
-//                   },
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       },
-//     ]);
-
-//     return apiResponse({
-//       res,
-//       data: modifyData[0],
-//       // data:  modifyData[0].data.sort((a, b) => a.name.localeCompare(b.name)),
-//       status: true,
-//       message: "Country league list fetched successfully",
-//       statusCode: StatusCodes.OK,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     if (error.response && error.response.status === 404) {
-//       return apiResponse({
-//         res,
-//         data: null,
-//         status: true,
-//         message: "No data found",
-//         statusCode: StatusCodes.OK,
-//       });
-//     } else {
-//       return apiResponse({
-//         res,
-//         status: false,
-//         message: "Internal server error",
-//         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-//       });
-//     }
-//   }
-// };
-
 const getCountryLeagueList = async (req, res, next) => {
   try {
     const { sport } = req.params;
@@ -731,6 +589,8 @@ const globalSearch = async (req, res, next) => {
         statusCode: StatusCodes.BAD_REQUEST,
       });
     }
+
+    data = data.reverse().slice(0, 10);
 
     return apiResponse({
       res,
