2197150e5632f3fc74f14486dc7dc8b50496468e
vijay <vijay.logicgo@gmail.com>
2024-07-23 14:56:31 +0530

    modify cricket matches api

--- cricket-custom-module/utils/utils.js
+++ cricket-custom-module/utils/utils.js
@@ -1,4 +1,15 @@
+import mongoose from "mongoose";
+
 export const getHostUrl = (req, middlePath) => {
     return req.protocol + "://" + req.get("host") + "/images/" +`${middlePath}/`;
+};
+
+export const validateObjectIds = (ids) => {
+  for (const [key, value] of Object.entries(ids)) {
+    if (!mongoose.Types.ObjectId.isValid(value)) {
+      return { isValid: false, message: `Invalid ${key}` };
+    }
+  }
+  return { isValid: true };
 };
   
